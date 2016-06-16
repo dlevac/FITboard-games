595628a51243fcde60a815f84ff1d8d3407c3303
DHeyman <d.heyman256@gmail.com>
2016-06-16 19:05:16 -0400

    Camera follows ball without spinning

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// basic script to have the camera follow the player while maintaining a constant rotation
+// Has assumption of 50-degree X-axis rotation hard-coded
+public class CamFollow : MonoBehaviour {
+    public Transform follow;
+    [SerializeField]
+    private float camDistance;
+    private Vector3 camVector;
+
+    void Start() {
+        camVector = new Vector3(0, camDistance / Mathf.Cos(Mathf.Deg2Rad * 50), camDistance / Mathf.Sin(Mathf.Deg2Rad * 50));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(follow.position.x, follow.position.y + camVector.y, follow.position.z - camVector.z);
+	}
+}
