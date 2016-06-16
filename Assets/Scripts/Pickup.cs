f878342aeffab46c065914153e253d2abecf023d
DHeyman <d.heyman256@gmail.com>
2016-06-16 19:04:32 -0400

    Pickup scorecounter externally readonly

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 
 public class Pickup : MonoBehaviour {
-    private static int totalCollected = 0;
+    private static int _score = 0;
+    public static int totalCollected { get { return _score;} }
+
+
 
     // When this pickup is touched by another collider, destroy this pickup and increase the count
     void OnTriggerEnter(Collider other) {
-        ++totalCollected;
+        ++_score;
         Destroy(gameObject);
     }
 }
