45afb2e8cd89c3615ea715088acda0203f3bbb8b
DHeyman <d.heyman256@gmail.com>
2016-06-16 19:04:59 -0400

    Score updating

---
+++ Assets/Scripts/ScoreUpdate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreUpdate : MonoBehaviour {
+    Text displayText;
+
+    void Start() {
+        displayText = GetComponent<Text>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        displayText.text = Pickup.totalCollected.ToString();
+	}
+}
